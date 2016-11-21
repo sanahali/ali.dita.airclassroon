2d2e1e8b2dcadabebd106e169c045acde8d29dd9
Sanah Ali <sanah13@vt.edu>
2016-11-20 21:35:32 -0500

    update

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,2 +1,3 @@
 fil = new Array();
 fil["0"]= "c-introduction-student.html@@@Welcome to Shanks 360!@@@In this room you&apos;ll find everything you need to conduct a productive and creative workshop, lecture, or conference...";
+fil["1"]= "t-computer.html@@@How to use the Mac Desktop Computer@@@The Mac Desktops are top of the line computers that are up to date with the latest Apple technology...";
--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -1,4 +1,5 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "c-introduction-student.html";
+fl["1"]= "t-computer.html";
 var doStem = false;searchLoaded = true;
